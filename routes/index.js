44b8f824c521983b7c1e9acb73ae529de87082a4
joecih <joecih@gmail.com>
2016-05-03 11:40:53 -0400

    Updates to layouts, added in requsers.js file, added in registration page

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var mongoUrl = "mongodb://localhost:27017/coffee";
+var mongoose = require('mongoose');
+mongoose.connect(mongoUrl);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Express'
-  });
+    res.render('index', {
+        title: 'Express'
+    });
+});
+
+router.get('/register', function(req, res, next) {
+    res.render('register', {});
+});
+
+router.post('/register', function(req, res, next) {
+    // the user posted: username, email, password, password2
+    res.json(req.body)
 });
 
 module.exports = router;
