5264f132854d5a8eba1330b0df528992f2b7389f
joecih <joecih@gmail.com>
2016-05-03 19:05:48 -0400

    Updated app.js, added in accounts.js, modified styling, created 2nd options nav

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var mongoUrl = "mongodb://localhost:27017/coffee";
 var mongoose = require('mongoose');
+var Account = require('../models/Accounts');
+var bcrypt = require('bcrypt-nodejs');
+
 mongoose.connect(mongoUrl);
 
 /* GET home page. */
@@ -12,12 +15,34 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/register', function(req, res, next) {
-    res.render('register', {});
+    res.render('register', {
+        page: 'register',
+        failure: req.query.failure
+    });
 });
+
+router.get('/options', function(req, res, next) {
+    res.render('options', {
+        username: req.session.username
+    });
+});
+
 
 router.post('/register', function(req, res, next) {
     // the user posted: username, email, password, password2
-    res.json(req.body)
+    if (req.body.password != req.body.password2) {
+        res.redirect('/register?failure=password')
+    } else {
+        var newAccount = new Account({
+            username: req.body.username,
+            password: bcrypt.hashSync(req.body.password),
+            emailAddress: req.body.email
+        })
+        console.log(newAccount);
+        newAccount.save();
+        req.session.username = req.body.username;
+        res.redirect('/options');
+    }
 });
 
 module.exports = router;
